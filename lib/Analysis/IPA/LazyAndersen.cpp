TristanSchmelcher/llvm-andersen

commit 78938f7b9a87fa630cc9dbe4546e6933adeb4dd0

Fix "Instruction used before executed" assertion failure with funny control flow.

lib/Analysis/IPA/LazyAndersen.cpp
@@ -22,6 +22,7 @@
 #include "LazyAndersenInstructionAnalyzer.h"
 #include "LazyAndersenRelationsGraphViewer.h"
 #include "LazyAndersenValuePrinter.h"
+#include "llvm/Analysis/Dominators.h"
 #include "llvm/Support/CommandLine.h"
 #include "llvm/Support/Debug.h"
 
@@ -35,9 +36,13 @@ cl::opt<bool> GraphRelations("andersen-graph-relations",
 
 char LazyAndersen::ID = 0;
 // TODO: What do these two bools mean?
-INITIALIZE_PASS(LazyAndersen, "lazy-andersen",
-                "Lazy Andersen's Algorithm for Points-To Analysis", false,
-                true)
+INITIALIZE_PASS_BEGIN(LazyAndersen, "lazy-andersen",
+                      "Lazy Andersen's Algorithm for Points-To Analysis", false,
+                      true)
+INITIALIZE_PASS_DEPENDENCY(DominatorTree)
+INITIALIZE_PASS_END(LazyAndersen, "lazy-andersen",
+                    "Lazy Andersen's Algorithm for Points-To Analysis", false,
+                    true)
 
 LazyAndersen::LazyAndersen()
   : ModulePass(ID), Data(0) {
@@ -55,7 +60,7 @@ DenseSet<const Value *> LazyAndersen::getPointsToSet(const Value *V) const {
 
 bool LazyAndersen::runOnModule(Module &M) {
   assert(!Data);
-  Data = InstructionAnalyzer::run(M);
+  Data = InstructionAnalyzer::run(this, M);
   if (NonLazy) {
     for (ValueInfo::Map::const_iterator i = Data->ValueInfos.begin();
          i != Data->ValueInfos.end(); ++i) {
@@ -75,6 +80,7 @@ void LazyAndersen::releaseMemory() {
 
 void LazyAndersen::getAnalysisUsage(AnalysisUsage &AU) const {
   AU.setPreservesAll();
+  AU.addRequired<DominatorTree>();
 }
 
 void LazyAndersen::print(raw_ostream &OS, const Module *M) const {

lib/Analysis/IPA/LazyAndersenInstructionAnalyzer.cpp
@@ -24,8 +24,10 @@
 #include "LazyAndersenReturnedToCallerRelation.h"
 #include "LazyAndersenStoredToRelation.h"
 #include "LazyAndersenValueInfo.h"
+#include "llvm/Analysis/Dominators.h"
 #include "llvm/IR/User.h"
 #include "llvm/IR/Value.h"
+#include "llvm/Pass.h"
 
 using namespace llvm;
 using namespace llvm::lazyandersen;
@@ -76,8 +78,8 @@ namespace {
   }
 }
 
-LazyAndersenData *InstructionAnalyzer::run(Module &M) {
-  return InstructionAnalyzer(M).Data;
+LazyAndersenData *InstructionAnalyzer::run(ModulePass *MP, Module &M) {
+  return InstructionAnalyzer(MP, M).Data;
 }
 
 void InstructionAnalyzer::visitReturnInst(ReturnInst &I) {
@@ -149,14 +151,37 @@ void InstructionAnalyzer::visitInstruction(Instruction &I) {
   cache(&I, analyzeUser(&I));
 }
 
-InstructionAnalyzer::InstructionAnalyzer(Module &M)
-  : Data(new LazyAndersenData()) {
+InstructionAnalyzer::InstructionAnalyzer(ModulePass *MP, Module &M)
+  : MP(MP), Data(new LazyAndersenData()) {
   for (Module::iterator i = M.begin(); i != M.end(); ++i) {
     Function &F(*i);
-    CurrentFunction = &F;
-    visit(F);
-    processPHINodes();
+    processFunction(F);
   }
+}
+
+void InstructionAnalyzer::processFunction(Function &F) {
+  CurrentFunction = &F;
+  // Visit the basic blocks in a depth-first traversal of the dominator tree.
+  // This ensures that we visit each instruction before each non-PHI use of it.
+  DomTreeNode *Root = MP->getAnalysis<DominatorTree>(F).getRootNode();
+  for (df_iterator<DomTreeNode *> i = df_begin(Root), End = df_end(Root);
+       i != End; ++i) {
+    visit(i->getBlock());
+  }
+  // Process all the PHI nodes.
+  for (PHINodeWorkVector::const_iterator i = PHINodeWork.begin();
+       i != PHINodeWork.end(); ++i) {
+    const PHINode *PHI = i->first;
+    ValueInfo *PHIAnalysis = i->second;
+    for (PHINode::const_op_iterator i = PHI->op_begin(); i != PHI->op_end();
+        ++i) {
+      ValueInfo *OperandAnalysis = analyzeValue(*i);
+      if (OperandAnalysis) {
+        new DependsOnRelation(PHIAnalysis, OperandAnalysis);
+      }
+    }
+  }
+  PHINodeWork.clear();
 }
 
 void InstructionAnalyzer::visitCallOrInvokeInst(Instruction &I,
@@ -185,22 +210,6 @@ void InstructionAnalyzer::visitCallOrInvokeInst(Instruction &I,
     cache(&I, ReturnedValueInfo);
   }
   // TODO: Record that the current function may call this function.
-}
-
-void InstructionAnalyzer::processPHINodes() {
-  for (PHINodeWorkVector::const_iterator i = PHINodeWork.begin();
-       i != PHINodeWork.end(); ++i) {
-    const PHINode *PHI = i->first;
-    ValueInfo *PHIAnalysis = i->second;
-    for (PHINode::const_op_iterator i = PHI->op_begin(); i != PHI->op_end();
-        ++i) {
-      ValueInfo *OperandAnalysis = analyzeValue(*i);
-      if (OperandAnalysis) {
-        new DependsOnRelation(PHIAnalysis, OperandAnalysis);
-      }
-    }
-  }
-  PHINodeWork.clear();
 }
 
 bool InstructionAnalyzer::analyzed(const Value *V) {

lib/Analysis/IPA/LazyAndersenInstructionAnalyzer.h
@@ -19,6 +19,7 @@
 
 namespace llvm {
   class LazyAndersenData;
+  class ModulePass;
 }
 
 namespace llvm {
@@ -29,12 +30,13 @@ namespace lazyandersen {
   class InstructionAnalyzer : public InstVisitor<InstructionAnalyzer> {
     typedef SmallVector<std::pair<const PHINode *, ValueInfo *>, 3>
         PHINodeWorkVector;
+    ModulePass *MP;
     PHINodeWorkVector PHINodeWork;
     LazyAndersenData *Data;
     Function *CurrentFunction;
 
   public:
-    static LazyAndersenData *run(Module &M);
+    static LazyAndersenData *run(ModulePass *MP, Module &M);
     void visitReturnInst(ReturnInst &I);
     void visitInvokeInst(InvokeInst &I);
     void visitAllocaInst(AllocaInst &I);
@@ -48,10 +50,10 @@ namespace lazyandersen {
     void visitInstruction(Instruction &I);
 
   private:
-    explicit InstructionAnalyzer(Module &M);
+    InstructionAnalyzer(ModulePass *MP, Module &M);
     void visitCallOrInvokeInst(Instruction &I,
         const CallOrInvokeInstWrapperInterface &W);
-    void processPHINodes();
+    void processFunction(Function &F);
     bool analyzed(const Value *V);
     ValueInfo *cache(const Value *V, ValueInfo *VI);
     ValueInfo *createValueInfo(const Value *V);

commit 576a9806c2244de321b787cb4ac27838fd996103

Fix getAnalysis assert failure introduced by 78938f7b9a87fa630cc9dbe4546e6933adeb4dd0.

lib/Analysis/IPA/LazyAndersen.cpp
@@ -50,6 +50,7 @@ LazyAndersen::LazyAndersen()
 }
 
 DenseSet<const Value *> LazyAndersen::getPointsToSet(const Value *V) const {
+  assert(Data->ValueInfos.count(V));
   DenseSet<const Value *> Out;
   if (Data->ValueInfos[V]) {
     Enumerator::enumerate(

lib/Analysis/IPA/LazyAndersenInstructionAnalyzer.cpp
@@ -155,7 +155,9 @@ InstructionAnalyzer::InstructionAnalyzer(ModulePass *MP, Module &M)
   : MP(MP), Data(new LazyAndersenData()) {
   for (Module::iterator i = M.begin(); i != M.end(); ++i) {
     Function &F(*i);
-    processFunction(F);
+    if (!F.isDeclaration()) {
+      processFunction(F);
+    }
   }
 }
 
@@ -163,6 +165,9 @@ void InstructionAnalyzer::processFunction(Function &F) {
   CurrentFunction = &F;
   // Visit the basic blocks in a depth-first traversal of the dominator tree.
   // This ensures that we visit each instruction before each non-PHI use of it.
+  // TODO: This skips (seemingly) unreachable code, so a future alias lookup on
+  // such code may break. We should depend on a transform pass that removes
+  // unreachable code.
   DomTreeNode *Root = MP->getAnalysis<DominatorTree>(F).getRootNode();
   for (df_iterator<DomTreeNode *> i = df_begin(Root), End = df_end(Root);
        i != End; ++i) {